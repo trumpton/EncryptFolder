e575916dc1d9d63ab1439b9be880035ac6778751
trumpton <git@trumpton.uk>
2024-10-19 21:43:32 +0100

    Updated version management

--- main.cpp
+++ main.cpp
@@ -5,35 +5,10 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc>1 && argv[1][0]=='-' && argv[1][1]!='\0') {
-
-        switch (argv[1][1]) {
-        case 'v':
-            std::cout << appHash().replace("v","").toLatin1().data() << "\n" ;
-            break ;
-        case 'l':
-            std::cout << libVersion().replace("v","").toLatin1().data() << "\n" ;
-            break ;
-        case 'd':
-            std::cout << buildDate().toLatin1().data() << "\n" ;
-            break ;
-        case 'a':
-            std::cout << appHash().replace("v","").toLatin1().data() << "\n" ;
-            std::cout << libVersion().replace("v","").toLatin1().data() << "\n" ;
-            std::cout << buildDate().toLatin1().data() << "\n" ;
-            break ;
-        default:
-            std::cout << "EncryptFolder -adlv\n -a - Show all\n -d - Build date\n -l - Library version\n -v - Application version\n" ;
-            break ;
-        }
-        return 1 ;
-
-    } else {
 
         QApplication a(argc, argv);
         MainWindow w;
         w.show();
 
         return a.exec();
-    }
 }
